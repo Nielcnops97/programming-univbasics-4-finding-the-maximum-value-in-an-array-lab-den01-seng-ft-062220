4909c4cc5171d7eaa1a5ee0f2d0d57038e168aef
Niel Cnops <nielcnops@outlook.com>
2020-06-12 20:23:37 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,8 @@
 def find_max_value(array)
   count = 0
+  max_value = -1
   while count < array.length do
-    if array[count] == max_value
+    if [count] == max_value
       return count
     end
     count += 1
